eb727b7047f26041d47e708aaac99d25a052ff6a
praisedev <62954475+praisedev@users.noreply.github.com>
2021-05-21 20:20:49 +0700

    Update config.js

--- config.js
+++ config.js
@@ -7,7 +7,7 @@ const {CookieAccessInfo, CookieJar, Cookie} = cookiejar
 
 let config = {
     httpprefix: 'https', port: 443,
-    serverName: 'siteproxy.netptop.workers.dev',
+    serverName: 'webproxy2.praise.ml',
 }
 let blockedSites = ['www.chase.com'] // accessing to chase.com was reported by google
 
